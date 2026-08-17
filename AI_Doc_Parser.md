89ad7a004854aea2ae91b8a1134047688cd953e3
Abhishek Choudhari <abhishek007coc@gmail.com>
2026-08-17 23:18:09 +0530

    Initial Commit

---
+++ AI_Doc_Parser.md
@@ -0,0 +1,4 @@
+Core Concepts:
+
+1. Define expected LLM Model Response Schemas using Pydantic Model Definitions.
+2. Use LLM Models client to Upload Invoice Files & Extract JSON based on Pydantic Model Definitions.
---
+++ AI_Doc_Parser.py
@@ -0,0 +1,52 @@
+import json
+import os
+
+from typing import List, Optional
+from pydantic import BaseModel, Field
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class LineItem(BaseModel):
+    description: str = Field(description="Description of the Product or Service")
+    quantity: Optional[float] = Field(default=None, description="Quantity Purchased of the Product or Service")
+    unit_price: Optional[float] = Field(default=None, description="Price Per Unit")
+    amount: float = Field(description="Total Price of this Line Item")
+    
+class InvoiceData(BaseModel):
+    vendor_name: str = Field(description="Name of the Company/Vendor Issuing the Invoice")
+    invoice_number: str = Field(description="Unique Identifier Number of the Invoice")
+    invoice_date: str = Field(description="Invoice Issue Date (DD-MM-YYYY)")
+    due_date: Optional[str] = Field(defualt=None, description="Payment Due Date of the Invoice (DD-MM-YYYY)")
+    currency: str = Field(defautl="INR", description="Currency Symbol or 3 Letter Code")
+    line_items: List[LineItem] = Field(description="List of all Product or Service Line Items in the Invoice")
+    subtotal: Optional[float] = Field(default=None, description="Subtotal of the Invoice Amount")
+    tax_amount: Optional[float] = Field(defualt=None, description="Total Tax Amount on Invoice Amount")
+    total_amount: float = Field(description="Final Total Amount of the Invoice")
+    
+    
+def parse_invoice_pdf(invoice_file_path):
+    client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+    
+    print(f"Uploading Invoice File: {invoice_file_path}")
+    uploaded_file = client.files.upload(file=invoice_file_path)
+    
+    llm_response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=[
+            uploaded_file,
+            "Extract all relevant invoice data accurately matching the requested schema."
+        ],
+        config=types.GenerateContentConfig(
+            response_mime_type="application/json",
+            response_schema=InvoiceData,
+            temperature=0.0
+        )
+    )
+    
+    return llm_response.parsed
+
+result = parse_invoice_pdf("invoice.pdf")
+print(result)
